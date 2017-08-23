d0bad13a3b5cc86b64ca2043085eb86f139c2fcc
marcelacsr <marcelacsr@id.uff.br>
2017-08-23 19:53:13 -0300

    Implement basic login2

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -28,7 +30,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    #assert is_logged_in?
+    assert is_logged_in?
   end
 
 end
