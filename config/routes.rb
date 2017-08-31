386144d4c06e6903adce25af781bdd849d0d11c7
marcelacsr <marcelacsr@id.uff.br>
2017-08-31 11:38:59 -0300

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help' #, as: 'helf' #?????
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
 
+  get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
+
 end
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
