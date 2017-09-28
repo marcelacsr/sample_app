d0d85d0e4e7fc31204b1f3c82ceae86db5e0fde6
marcelacsr <marcelacsr@id.uff.br>
2017-09-28 12:42:17 -0300

    Correcoes do rubocop 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,11 +75,10 @@ class UsersController < ApplicationController
 
   # Confirms a logged-in user.
   def logged_in_user
-    unless logged_in?
+    return if logged_in?
     store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
-      end
   end
 
   # Confirms the correct user.
